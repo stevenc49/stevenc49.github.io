9ae51256519f6d884ec3ffa47765020351e9044f
Steven Cheong <stevenc49@gmail.com>
2020-06-08 16:16:36 -0700

    (_code/arrays/searchInRotatedArray.py)

--- _code/arrays/searchInRotatedArray.py
+++ _code/arrays/searchInRotatedArray.py
@@ -1,6 +1,19 @@
-nums = [4,5,6,7,0,1,2]
-target = 5
+nums = [4,5,6,7,2,3]
+target = 1
 
 def search(nums, target):
 
-    
+    left = 0
+    right = len(nums)-1
+
+    # this loop gets left to the smallest element
+    while left<right:
+
+        mid = (left + right) // 2
+        if nums[mid] > nums[right]:
+            left = mid+1
+        else:
+            right = mid
+
+
+print( search(nums, target) )
