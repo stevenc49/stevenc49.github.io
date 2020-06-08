ab2f52143ebbe8afd9f2c3ef4dd77ac00a124d72
Steven Cheong <stevenc49@gmail.com>
2020-06-07 21:35:43 -0700

    (_code/dynamic programming/coinChange2.py)

---
+++ _code/dynamic programming/coinChange2.py
@@ -0,0 +1,19 @@
+amount = 5
+coins = [1,2,5]
+
+def change(amount, coins):
+    N = len(coins)
+    if N == 0: return int(N == amount)
+    
+    dp_sum = [[0] * N for _ in range(amount + 1)]
+    for i in range(N): dp_sum[0][i] = 1
+    
+    for i,j in product(range(amount), range(N)):
+        dp_sum[i+1][j] = dp_sum[i+1][j-1]
+        if i+1 - coins[j] >= 0:
+            dp_sum[i+1][j] += dp_sum[i+1-coins[j]][j]           
+                
+    return dp_sum[-1][-1]
+
+
+print( change( amount, coins ) )
