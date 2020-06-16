9b9732b58de5e2709a492da1504cbf4b7dcb91bb
Steven Cheong <stevenc49@gmail.com>
2020-06-15 17:29:20 -0700

    (_code/graphs/cheapestFlightsWithinKStopsDijkstra.py)

--- _code/graphs/cheapestFlightsWithinKStopsDijkstra.py
+++ _code/graphs/cheapestFlightsWithinKStopsDijkstra.py
@@ -1,33 +1,52 @@
+from collections import deque
+
+'''
+https://www.youtube.com/watch?v=Jbb9qNIAyg0
+'''
 import collections
 
 n = 3
-edges = [[0,1,100],[1,2,200],[0,2,500]]
+flights = [[0,1,100],[1,2,200],[0,2,500]]
 src = 0
-dst = 2
+dest = 2
 k = 1
 
 
 
-def findCheapestPrice(n, flights, src, dst, K):
+def findCheapestPrice(n, flights, src, dest, K):
 
-    if src == dst: return 0
+    # d = collections.defaultdict(list)
+    # seen = collections.defaultdict(lambda: float('inf'))
 
-    d, seen = collections.defaultdict(list), collections.defaultdict(lambda: float('inf'))
-    for u, v, p in flights:
-        d[u] += [(v, p)]
+    adj = [[] for _ in range(n)]
+    output = [float('inf') for _ in range(n)]
+    output[src] = 0
 
-    q = [(src, -1, 0)]
-    
-    while q:
-        pos, k, cost = q.pop(0)
-        if pos == dst or k == K: continue 
-        for nei, p in d[pos]:
-            if cost + p >= seen[nei]:
-                continue
-            else:
-                seen[nei] = cost+p
-                q += [(nei, k+1, cost+p)]
-            
-    return seen[dst] if seen[dst] < float('inf') else -1
+    # add to adj list where key is src, and value is destination+cost
+    for u, v, w in flights:
+        adj[u] += [(v, w)]
 
-print( findCheapestPrice(n, edges, src, dst, k) )
+
+    graph = deque()
+    graph.append((src, -1))     # where -1 is the number of stops
+
+    while graph:
+
+        u, stop = graph.popleft()
+        if stop >= K:
+            continue
+
+        for v, w in adj[u]:
+            output[v] = output[u]+w
+            graph.append((v,stop+1))
+        
+    if output[dest] == float('inf'):
+        return -1
+    else:
+        return output[dest]
+
+
+    print(d)
+    print(seen)
+
+print( findCheapestPrice(n, flights, src, dest, k) )
