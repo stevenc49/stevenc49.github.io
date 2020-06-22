8008a1616f8f67f5dbef9875fb646cec948e335b
Steven Cheong <stevenc49@gmail.com>
2020-06-22 15:10:36 -0700

    (_code/permutation/subsets.py)

--- _code/permutation/subsets.py
+++ _code/permutation/subsets.py
@@ -1,23 +1,38 @@
 nums = [1,2,3]
 
 
-def subsets(nums):
-    def backtrack(first = 0, curr = []):
-        # if the combination is done
-        if len(curr) == k:  
-            output.append(curr[:])
-        for i in range(first, n):
-            # add nums[i] into the current combination
-            curr.append(nums[i])
-            # use next integers to complete the combination
-            backtrack(i + 1, curr)
-            # backtrack
-            curr.pop()
+# def subsets(nums):
+
+#     def backtrack(first = 0, curr = []):
+#         # if the combination is done
+#         if len(curr) == k:  
+#             output.append(curr[:])
+#         for i in range(first, n):
+#             # add nums[i] into the current combination
+#             curr.append(nums[i])
+#             # use next integers to complete the combination
+#             backtrack(i + 1, curr)
+#             # backtrack
+#             curr.pop()
     
-    output = []
-    n = len(nums)
-    for k in range(n + 1):
-        backtrack()
-    return output
+#     output = []
+#     n = len(nums)
+#     for k in range(n + 1):
+#         backtrack()
+#     return output
+
+
+def subsets(nums):
+    subsets = []
+    
+    def generateSubsets(index, nums, current, subsets):
+        subsets.append(current[:])
+        for i in range(index, len(nums)):
+            current.append(nums[i])
+            generateSubsets(i + 1, nums, current, subsets)
+            current.remove(current[-1])
+            
+    generateSubsets(0, nums, [], subsets)
+    return subsets
 
 print(subsets(nums))
