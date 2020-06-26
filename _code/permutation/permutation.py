6e3fc201b6a660dfaf9dc23c1b15cf245734941e
Steven Cheong <stevenc49@gmail.com>
2020-06-26 12:23:27 -0700

    (_code/permutation/permutation.py)

--- _code/permutation/permutation.py
+++ _code/permutation/permutation.py
@@ -30,7 +30,8 @@ def permute(nums):
                     continue
                 currentList.append(nums[i])
                 backtrack( currentList, allPermutations, nums )
-                currentList.remove(currentList[-1])
+                currentList.remove(currentList[-1])   # currentList.pop() also does the same thing
+
 
     allPermutations = []
     backtrack([], allPermutations, nums)
