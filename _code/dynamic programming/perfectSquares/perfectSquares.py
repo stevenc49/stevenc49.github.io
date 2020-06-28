c8dfb252511f0f1d293fc94b829662f7e3374111
Steven Cheong <stevenc49@gmail.com>
2020-06-27 21:33:31 -0700

    (_code/dynamic programming/perfectSquares/perfectSquares.py)

--- _code/dynamic programming/perfectSquares/perfectSquares.py
+++ _code/dynamic programming/perfectSquares/perfectSquares.py
@@ -4,8 +4,10 @@ n = 7
 
 def numSquares(n):
 
-    squares = [i**2 for i in range(0, math.sqrt(n))]
+    squares = [i**2 for i in range(0, int(math.sqrt(n))+1) ]
+    dp = [float(inf)] * n
 
     print(squares)
+    print(dp)
 
 print( numSquares(n) )
