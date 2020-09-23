b20e50e2d33b9990fe412015dcc9ca07d80c41de
Steven Cheong <stevenc49@gmail.com>
2020-09-22 22:20:22 -0700

    (_problems/1template copy.md site-management/generateCPfeed.py pages/lists/solvedLastModified.md)

---
+++ site-management/generateCPfeed.py
@@ -0,0 +1,29 @@
+import pprint
+import os, subprocess
+import re
+from fileutils import FileUtil
+
+'''
+    This script creates a chinesepod xml feed for beyondpod
+'''
+
+ROOT_DIR = "/home/steve/workspace/stevenc49.github.io/"
+SITE_DIR = ROOT_DIR + "_site"
+
+
+
+def main():
+
+
+
+    # write to site dir
+    FileUtil.saveToFile(sb, SITE_DIR + "/feed.xml")
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    main()
