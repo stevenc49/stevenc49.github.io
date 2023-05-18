a3a3f7670ad481f545380345bff315b0aac2d7c4
scheong <steven.cheong@telus.com>
2023-05-18 11:29:48 -0600

    find anagrams in string with sliding window template

--- _code/sliding_window/find_all_anagrams_in_a_string.py
+++ _code/sliding_window/find_all_anagrams_in_a_string.py
@@ -1,40 +1,83 @@
+'''
+Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Input: s = "cbaebabacd", p = "abc"
+Output: [0,6]
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
+
+'''
+
 from collections import defaultdict
 from typing import List
 
-
+'''
+    using sliding window template
+'''
 def findAnagrams(s: str, p: str) -> List[int]:
 
-
-    freq = defaultdict(int)
     res = []
 
-    if (len(p) > len(s)):
-        return res
+    for i in range(len(s) - len(p)+1):
 
-    # build map of freq in p
-    for char in p:
-        freq[char] +=1
+        # build map of p
+        freq = defaultdict(int)
+        for char in p:
+            freq[char] += 1
+        print(' ' + str(freq))
 
-    # start with initial pass (so later pass can include entire slide in/slide out)
-    for i in range(0, len(p)-1):
-        if s[i] in freq:
-            freq[s[i]] -= 1
+        for j in range(i, i+len(p)):
 
+            print(s[j], end='')
+            if s[j] in freq:
+                freq[s[j]] -= 1
 
-    for i in range(-1, len(s)-len(p)+1):
+            print(' ' + str(freq))
+            if not any(freq.values()):
+                print('adding ' + str(j))
+                res.append(i)
 
-        # if slide in, subtract
-        if i+len(p)<len(s) and s[i+len(p)] in freq:
-            freq[s[i+len(p)]] -= 1
-
-        # if slide out, add
-        if i>-1 and s[i] in freq:
-            freq[s[i]] += 1
-
-        if not any(freq.values()):
-            res.append(i+1)     # because in this iteration, 'i' will be behind by 1 when the slide in char will be p ahead
+        print()
 
     return res
+
+
+# def findAnagrams(s: str, p: str) -> List[int]:
+#
+#
+#     freq = defaultdict(int)
+#     res = []
+#
+#     if (len(p) > len(s)):
+#         return res
+#
+#     # build map of freq in p
+#     for char in p:
+#         freq[char] +=1
+#
+#     # start with initial pass (so later pass can include entire slide in/slide out)
+#     for i in range(0, len(p)-1):
+#         if s[i] in freq:
+#             freq[s[i]] -= 1
+#
+#
+#     for i in range(-1, len(s)-len(p)+1):
+#
+#         # if slide in, subtract
+#         if i+len(p)<len(s) and s[i+len(p)] in freq:
+#             freq[s[i+len(p)]] -= 1
+#
+#         # if slide out, add
+#         if i>-1 and s[i] in freq:
+#             freq[s[i]] += 1
+#
+#         if not any(freq.values()):
+#             res.append(i+1)     # because in this iteration, 'i' will be behind by 1 when the slide in char will be p ahead
+#
+#     return res
 
 
 # def findAnagrams(s: str, p: str) -> List[int]:
