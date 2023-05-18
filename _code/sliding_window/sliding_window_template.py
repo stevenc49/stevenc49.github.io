bda403f634d9d9cb7d54fad411e37c31f1afd887
scheong <steven.cheong@telus.com>
2023-05-17 19:33:58 -0600

    new

--- _code/sliding_window/sliding_window_template.py
+++ _code/sliding_window/sliding_window_template.py
@@ -1,15 +1,39 @@
 
+'''
 
-arr = [1,2,3,4,5,6]
-k = 3
+    Find the max sum subarray with size k
+    [4,2,1,7,8,1,2,8,1,0] k=3
 
-for i in range(len(arr) - k + 1):
+    1,7,8 produces 16
+'''
 
-    sum = 0
+def maxSubarray(arr, k):
 
-    for j in range(i, i+k):
+    maxSum = 0
 
-        print(arr[j])
+    for i in range(len(arr) - k + 1):
 
-    print()
+        sum = 0
 
+        for j in range(i, i+k):
+
+            sum += arr[j]
+            print(arr[j])
+
+            # if sum>maxSum:
+            #     maxSum = sum
+
+            print()
+
+    return maxSum
+
+
+def main():
+    arr = [1, 2, 3, 4, 5, 6]
+    k = 3
+
+    res = maxSubarray(arr, k)
+    print(res)
+
+if __name__ == "__main__":
+    main()
